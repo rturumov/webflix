818fe576aa03901668ad2755590c4033cecb1934
inviista <aldiyar_sadykov@mail.ru>
2024-10-16 01:10:37 +0500

    Initial commit

--- src/routes/cartoons.lazy.tsx
+++ src/routes/cartoons.lazy.tsx
@@ -8,5 +8,47 @@ export const Route = createLazyFileRoute('/cartoons')({
 
 function Cartoons() {
     const { t } = useTranslation();
-    return <div className="p-2">{t('Cartoons')}</div>
+    const topCartoons = [
+        {
+            title: "Головоломка 2",
+            description: "Продолжение приключений Райлли и ее эмоций, которые сталкиваются с новыми вызовами и открытиями.",
+            image: "https://bluraymania.com.ua/cdn/shop/articles/multfilm-golovolomka-2-preodolel-otmetku-v-900-mln-doll-701482.jpg?v=1719654314&width=2200", // Замените на реальный URL изображения
+        },
+        {
+            title: "Муфаса: Король лев",
+            description: "Приквел к легендарному фильму о Муфасе и его приключениях до событий 'Короля льва'.",
+            image: "https://www.caravan.kz/wp-content/uploads/images/700702.jpg", // Замените на реальный URL изображения
+        },
+        {
+            title: "Человек-паук: Через вселенные 3",
+            description: "Третий фильм о Человеке-пауке, который исследует новые вселенные и сталкивается с новыми противниками.",
+            image: "https://s1.stc.all.kpcdn.net/afisha/msk/wp-content/uploads/sites/5/2023/02/chelovek-pauk.jpg", // Замените на реальный URL изображения
+        },
+        {
+            title: "Властелины вселенной",
+            description: "Приключения героев, борющихся за спасение вселенной от злодеев.",
+            image: "https://s1.stc.all.kpcdn.net/afisha/msk/wp-content/uploads/sites/5/2023/02/vlasteliny-vselennoj.jpg", // Замените на реальный URL изображения
+        },
+        {
+            title: "Гадкий я 4",
+            description: "Продолжение истории Гру и его забавных приключений с Миньонами.",
+            image: "https://ss.sport-express.ru/userfiles/materials/202/2025021/large.jpg", // Замените на реальный URL изображения
+        },
+    ];
+    return <div id="top-cartoons" style={{padding: '20px 0'}}>
+        <h2 className="text-center font-bold"
+            style={{fontSize: '2rem', marginBottom: '24px'}}>{t('Топ мультфильмов 2024 года')}</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {topCartoons.map((cartoons, index) => (
+                <div key={index} className="border rounded-md overflow-hidden shadow-lg">
+                    <img src={cartoons.image} alt={cartoons.title} className="w-full h-48 object-cover"/>
+                    <div className="p-4">
+                        <h3 className="text-lg font-bold">{cartoons.title}</h3>
+                        <p className="text-gray-600">{cartoons.description}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    </div>
 }
+export default Cartoons;
--- src/routes/movies.lazy.tsx
+++ src/routes/movies.lazy.tsx
@@ -8,5 +8,48 @@ export const Route = createLazyFileRoute('/movies')({
 
 function Movies() {
     const { t } = useTranslation();
-    return <div className="p-2">{t('Movies')}</div>
+    const topMovies = [
+        {
+            title: "Дэдпул 3",
+            description: "«Дэдпу́л и Росома́ха» — американский полнометражный художественный супергеройский фильм 2024 года, снятый по мотивам комиксов компании Marvel Comics о Дэдпуле и Росомахе. Снят компаниями Marvel Studios, Maximum Effort и 21 Laps Entertainment и распространяется Walt Disney Studios Motion Pictures.",
+            image: "https://www.soyuz.ru/public/uploads/files/2/7628045/202309212039538137c82a05.jpg", // Замените на реальный URL изображения
+        },
+        {
+            title: "Планета обезьян: Новое царство",
+            description: "Плане́та обезья́н: Но́вое ца́рство» — американский научно-фантастический боевик режиссёра Уэса Болла и сценаристов Джоша Фридмана, Рика Джаффы, Аманды Сильвер и Патрика Эйсона. Сиквел картины «Планета обезьян: Война» 2017 года и четвёртая часть перезапущенной франшизы «Планета обезьян». В главной роли Оуэн Тиг.",
+            image: "https://focus.ua/static/storage/thumbs/920x465/d/50/60410627-e1ea529ddefcc6f2629654443f15c50d.jpg?v=8804_1", // Замените на реальный URL изображения
+        },
+        {
+            title: "Ребел-Ридж",
+            description: "«Ребел-Ридж» — будущий американский триллер режиссёра Джереми Солнье по его же сценарию. Главные роли в фильме исполнили Аарон Пьер, Аннасофия Робб, Джеймс Бэдж Дэйл, Джеймс Кромвелл и Дон Джонсон. Премьера фильма на Netflix запланирована на 6 сентября 2024 года. ",
+            image: "https://img.championat.ru/s/732x488/news/big/f/l/rebel-ridzh-2024-obzor-otzyv-vpechatleniya_17258792271243921511.jpg", // Замените на реальный URL изображения
+        },
+        {
+            title: "Годзилла и Конг: Новая империя",
+            description: "«Годзи́лла и Конг: Но́вая импе́рия» — американский фильм о монстрах режиссёра Адама Вингарда.",
+            image: "https://ss.sport-express.ru/userfiles/materials/199/1996118/volga.jpg", // Замените на реальный URL изображения
+        },
+        {
+            title: "Охотники за привидениями: Леденящий ужас",
+            description: "«Охо́тники за привиде́ниями: Леденя́щий у́жас» — американский фантастический комедийный фильм, снятый Гилом Кинаном в соавторстве с Джейсоном Райтманом. Продолжение фильмов «Охотники за привидениями», «Охотники за привидениями 2» и «Охотники за привидениями: Наследники», четвёртая часть по хронологии и пятая по счёту.",
+            image: "https://www.soyuz.ru/public/uploads/files/2/7633317/20240325121249a091cb6421.jpg", // Замените на реальный URL изображения
+        },
+    ];
+
+    return <div id="top-movies" style={{padding: '12px 0'}}>
+        <h2 className="text-center font-bold"
+            style={{fontSize: '2rem', marginBottom: '24px'}}>{t('Топ фильмов 2024 года')}</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {topMovies.map((movie, index) => (
+                <div key={index} className="border rounded-md overflow-hidden shadow-lg">
+                    <img src={movie.image} alt={movie.title} className="w-full h-48 object-cover"/>
+                    <div className="p-4">
+                        <h3 className="text-lg font-bold">{movie.title}</h3>
+                        <p className="text-gray-600">{movie.description}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    </div>
 }
+export default Movies;
